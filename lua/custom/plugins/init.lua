71ddbe996ce28598c1f235d1f91419eb96d184f2
raynertjx <rayner742@gmail.com>
2026-07-06 10:41:32 +0800

    add sidekick

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,6 +30,30 @@ return {
       { '<leader>bo', '<cmd>BufferLineCloseOthers<CR>', desc = '[B]uffer close [O]thers' },
     },
   },
+  { -- Sidekick: Neovim UI host for AI CLIs (OpenCode, Claude, etc.)
+    'folke/sidekick.nvim',
+    opts = {
+      nes = { enabled = false },
+      cli = {
+        mux = {
+          backend = 'tmux',
+          enabled = true,
+          create = 'terminal',
+        },
+      },
+    },
+    keys = {
+      { '<c-.>', function() require('sidekick.cli').focus() end, desc = 'Sidekick Focus', mode = { 'n', 't', 'i', 'x' } },
+      { '<leader>aa', function() require('sidekick.cli').toggle() end, desc = 'Sidekick Toggle CLI' },
+      { '<leader>as', function() require('sidekick.cli').select() end, desc = 'Select CLI' },
+      { '<leader>ad', function() require('sidekick.cli').close() end, desc = 'Detach CLI Session' },
+      { '<leader>at', function() require('sidekick.cli').send({ msg = '{this}' }) end, mode = { 'x', 'n' }, desc = 'Send This' },
+      { '<leader>af', function() require('sidekick.cli').send({ msg = '{file}' }) end, desc = 'Send File' },
+      { '<leader>av', function() require('sidekick.cli').send({ msg = '{selection}' }) end, mode = { 'x' }, desc = 'Send Visual Selection' },
+      { '<leader>ap', function() require('sidekick.cli').prompt() end, mode = { 'n', 'x' }, desc = 'Sidekick Select Prompt' },
+      { '<leader>ao', function() require('sidekick.cli').toggle({ name = 'opencode', focus = true }) end, desc = 'Sidekick Toggle OpenCode' },
+    },
+  },
   { -- kanagawa.nvim (installed, not active by default)
     'rebelot/kanagawa.nvim',
     priority = 1000,
