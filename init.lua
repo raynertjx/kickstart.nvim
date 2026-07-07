02e422f3d65474d9126bb844e8226c5978eb8fba
raynertjx <rayner742@gmail.com>
2026-07-07 10:52:53 +0800

    add buffer commands

--- init.lua
+++ init.lua
@@ -100,9 +100,7 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -201,6 +199,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>bb', '<cmd>b#<CR>', { desc = 'Switch to previous [B]uffer' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -463,6 +462,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+      vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[S]earch [G]it [S]tatus' })
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,6 +11,7 @@ return {
     opts = {
       options = {
         separator_style = 'slant',
+        numbers = 'ordinal',
         show_buffer_close_icons = true,
         diagnostics = 'nvim_lsp',
         offsets = {
@@ -28,6 +29,16 @@ return {
       { '<S-l>', '<cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
       { '<leader>bd', '<cmd>bdelete<CR>', desc = '[B]uffer [D]elete' },
       { '<leader>bo', '<cmd>BufferLineCloseOthers<CR>', desc = '[B]uffer close [O]thers' },
+      { '<leader>1', '<cmd>BufferLineGoToBuffer 1<CR>', desc = 'Go to buffer 1' },
+      { '<leader>2', '<cmd>BufferLineGoToBuffer 2<CR>', desc = 'Go to buffer 2' },
+      { '<leader>3', '<cmd>BufferLineGoToBuffer 3<CR>', desc = 'Go to buffer 3' },
+      { '<leader>4', '<cmd>BufferLineGoToBuffer 4<CR>', desc = 'Go to buffer 4' },
+      { '<leader>5', '<cmd>BufferLineGoToBuffer 5<CR>', desc = 'Go to buffer 5' },
+      { '<leader>6', '<cmd>BufferLineGoToBuffer 6<CR>', desc = 'Go to buffer 6' },
+      { '<leader>7', '<cmd>BufferLineGoToBuffer 7<CR>', desc = 'Go to buffer 7' },
+      { '<leader>8', '<cmd>BufferLineGoToBuffer 8<CR>', desc = 'Go to buffer 8' },
+      { '<leader>9', '<cmd>BufferLineGoToBuffer 9<CR>', desc = 'Go to buffer 9' },
+      { '<leader>0', '<cmd>BufferLineGoToBuffer 10<CR>', desc = 'Go to buffer 10' },
     },
   },
   { -- Sidekick: Neovim UI host for AI CLIs (OpenCode, Claude, etc.)
